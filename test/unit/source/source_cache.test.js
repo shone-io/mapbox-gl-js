13424fd10ce6701dc648d9512b26ede0279c2961
Molly Lloyd <molly@mapbox.com>
2017-09-29 17:46:51 -0700

    fix incorrect child tile removal

--- test/unit/source/source_cache.test.js
+++ test/unit/source/source_cache.test.js
@@ -720,12 +720,6 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
         stateCache[idealTiles[0].id] = 'loaded';
         const retained = sourceCache._updateRetainedTiles(idealTiles, 1);
         t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [
-            // checks all child tiles to see if they're loaded before loading parent
-            new TileCoord(2, 0, 2),
-            new TileCoord(2, 1, 2),
-            new TileCoord(2, 0, 3),
-            new TileCoord(2, 1, 3),
-
             // when child tiles aren't found, check and request parent tile
             new TileCoord(0, 0, 0)
         ]);
@@ -765,11 +759,6 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
 
         sourceCache._updateRetainedTiles([idealTile], 2);
         t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [
-            // all children
-            new TileCoord(3, 0, 0), // not found
-            new TileCoord(3, 1, 0), // not found
-            new TileCoord(3, 0, 1), // not found
-            new TileCoord(3, 1, 1), // not found
             // parents
             new TileCoord(1, 0, 0), // not found
             new TileCoord(0, 0, 0)  // not found
@@ -808,11 +797,6 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
         const retained = sourceCache._updateRetainedTiles([idealTile], 1);
 
         t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [
-            // all children
-            new TileCoord(2, 0, 2), // not found
-            new TileCoord(2, 1, 2), // not found
-            new TileCoord(2, 0, 3), // not found
-            new TileCoord(2, 1, 3), // not found
             // parents
             new TileCoord(0, 0, 0), // found
         ]);
@@ -862,11 +846,6 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
         const getTileSpy = t.spy(sourceCache, 'getTile');
         let retained = sourceCache._updateRetainedTiles([idealTile], 1);
         t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [
-            // all children
-            new TileCoord(2, 0, 0),
-            new TileCoord(2, 1, 0),
-            new TileCoord(2, 0, 1),
-            new TileCoord(2, 1, 1),
             // parent
             new TileCoord(0, 0, 0)
         ]);
@@ -884,7 +863,7 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
         addTileSpy.restore();
         getTileSpy.restore();
         // remove child tile and check that it only uses parent tile
-        sourceCache._tiles['2'] = null;
+        delete sourceCache._tiles['2'];
         retained = sourceCache._updateRetainedTiles([idealTile], 1);
 
         t.deepEqual(retained, {
@@ -916,13 +895,7 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
         const getTileSpy = t.spy(sourceCache, 'getTile');
         const retained = sourceCache._updateRetainedTiles([idealTile], 2);
 
-        t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [
-            // all children
-            new TileCoord(3, 0, 0),
-            new TileCoord(3, 1, 0),
-            new TileCoord(3, 0, 1),
-            new TileCoord(3, 1, 1)
-        ], 'doesn\'t request parent tiles bc they are lower than minzoom');
+        t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [], 'doesn\'t request parent tiles bc they are lower than minzoom');
 
         t.deepEqual(retained, {
             // ideal tile id (2, 0, 0)
@@ -975,11 +948,6 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
         const getTileSpy = t.spy(sourceCache, 'getTile');
         sourceCache._updateRetainedTiles(idealTiles, 8);
         t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [
-            // child tiles
-            new TileCoord(9, 0, 0),
-            new TileCoord(9, 1, 0),
-            new TileCoord(9, 0, 1),
-            new TileCoord(9, 1, 1),
             // parent tile ascent
             new TileCoord(7, 0, 0),
             new TileCoord(6, 0, 0),
@@ -989,11 +957,7 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
             new TileCoord(2, 0, 0),
             new TileCoord(1, 0, 0),
             new TileCoord(0, 0, 0),
-            // second ideal tile children, no parent ascent
-            new TileCoord(9, 2, 0),
-            new TileCoord(9, 3, 0),
-            new TileCoord(9, 2, 1),
-            new TileCoord(9, 3, 1)
+
         ], 'only ascends up a tile pyramid once');
 
         getTileSpy.reset();
@@ -1006,22 +970,12 @@ test('SourceCache#_updateRetainedTiles', (t)=> {
 
         sourceCache._updateRetainedTiles(idealTiles, 8);
         t.deepEqual(getTileSpy.getCalls().map((c)=>{ return c.args[0]; }), [
-            // child tiles
-            new TileCoord(9, 0, 0),
-            new TileCoord(9, 1, 0),
-            new TileCoord(9, 0, 1),
-            new TileCoord(9, 1, 1),
             // parent tile ascent
             new TileCoord(7, 0, 0),
             new TileCoord(6, 0, 0),
             new TileCoord(5, 0, 0),
             new TileCoord(4, 0, 0), // tile is loaded, stops ascent
 
-            // second ideal tile children, no parent ascent
-            new TileCoord(9, 2, 0),
-            new TileCoord(9, 3, 0),
-            new TileCoord(9, 2, 1),
-            new TileCoord(9, 3, 1)
         ], 'ascent stops if a loaded parent tile is found');
 
         getTileSpy.restore();
