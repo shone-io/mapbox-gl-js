ae132e28e12381c4e29e563f9814b5877824f44d
Molly Lloyd <molly@mapbox.com>
2017-10-16 17:53:55 -0700

    refactor based on review comments

--- src/source/tile.js
+++ src/source/tile.js
@@ -81,7 +81,7 @@ class Tile {
     maskedBoundsBuffer: ?VertexBuffer;
     maskedIndexBuffer: ?IndexBuffer;
     segments: ?SegmentVector;
-    prepared: ?boolean
+    needsHillshadePrepare: ?boolean
     request: any;
     texture: any;
     demTexture: ?Texture;
